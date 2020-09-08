29d5a02d7763888305dfc0b6de2c171959da7218
katylahv <katylahv@gmail.com>
2020-09-08 15:25:02 -0500

    added logo svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Link } from 'react-router-dom';
+import Projects from './projects.js';
+import Cv from './cv.js';
 
 function App() {
   return (
+   <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Route path="/projects" component={Projects}/>
+      <Route path="/cv" component={Cv}/>
+      
+      <div className="navigation">
+        <img src={logo} className="logo-image" alt="Logo Image"/>
+        <div className="navigation-sub">
+
+          <Link to="/projects" className="item">Projects</Link>
+          <Link to="/cv" className="item">CV</Link>
+
+        </div>
+      </div>
     </div>
+   </BrowserRouter>
   );
 }
 
