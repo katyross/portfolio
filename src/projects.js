83bf7416e59bdb86d2a2c57f96dc552be5013d1f
Katherine Ross <katylahv@gmail.com>
2020-09-08 21:30:59 -0500

    project thumbnails added

--- src/projects.js
+++ src/projects.js
@@ -5,11 +5,18 @@ function Projects(props){
     return(
         <div>
             <Thumbnails
-        link="/sidework"
-        image="https://picsum.photos/200/200.jpg"
-        title="$IDEWORK"
-        category="full-stack"
-      />
+                link="/sidework"
+                image="https://picsum.photos/200/200.jpg"
+                title="$IDEWORK"
+                category="full-stack"
+            />
+
+            <Thumbnails
+                link="/portfolio"
+                image="https://picsum.photos/200/200.jpg"
+                title="portfolio"
+                category="front end"
+            />
         </div>
     )
 }
